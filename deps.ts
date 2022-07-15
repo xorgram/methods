4f3b24fbaadb11bfdd5317a0b85029cf3151db73
Roj <ez@roj.im>
2022-07-15 10:24:50 +0300

    refactor: use the bigInt from grm

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { bigInt } from "https://deno.land/x/biginteger@v0.1.3/mod.ts";
+export { bigInt } from "https://deno.land/x/grm@v0.1.5/deps.ts";
 export { Api, TelegramClient } from "https://deno.land/x/grm@v0.1.5/mod.ts";
