2c684f57ca1da7ca73bc10dc30621d8a8a303e9e
Roj <ez@roj.im>
2022-08-09 12:25:40 +0300

    chore: update grm

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { bigInt } from "https://deno.land/x/grm@v0.3.0/deps.ts";
-export { Api, TelegramClient } from "https://deno.land/x/grm@v0.3.0/mod.ts";
+export { bigInt } from "https://deno.land/x/grm@v0.3.2/deps.ts";
+export { Api, TelegramClient } from "https://deno.land/x/grm@v0.3.2/mod.ts";
