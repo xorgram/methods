655d9cc822c0d09335974ae1967ec9d6d183e156
Roj <ez@roj.im>
2023-01-20 15:07:59 +0300

    chore: bump grm

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { bigInt } from "https://deno.land/x/grm@0.5.2/deps.ts";
-export { Api, TelegramClient } from "https://deno.land/x/grm@0.5.2/mod.ts";
+export { bigInt } from "https://deno.land/x/grm@0.5.4/deps.ts";
+export { Api, TelegramClient } from "https://deno.land/x/grm@0.5.4/mod.ts";
